d74c8e18b0119a6dcea1d51d1a2d60c95b7f23af
Matthias Kost <matthias.kost@uni-ulm.de>
2026-04-10 16:05:28 +0200

    make sure dev is a default pip extra environment if not specified else

--- scripts/03_install_package.sh
+++ scripts/03_install_package.sh
@@ -6,18 +6,19 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 PYTHON_BIN="${PYTHON_BIN:-python}"
 PIP_EXTRAS="${PIP_EXTRAS:-}"
 INSTALL_TEST_DEPS="${INSTALL_TEST_DEPS:-1}"
+DEFAULT_PIP_EXTRAS="dev"
+INSTALL_EXTRAS="${DEFAULT_PIP_EXTRAS}"
+
+if [[ -n "${PIP_EXTRAS}" ]]; then
+    INSTALL_EXTRAS="${DEFAULT_PIP_EXTRAS},${PIP_EXTRAS}"
+fi
 
 echo "==> PHOENIX package installation"
 echo
 echo "Installing from : ${ROOT_DIR}"
 echo "Python command  : ${PYTHON_BIN}"
-
-if [[ -n "${PIP_EXTRAS}" ]]; then
-    echo "Extras          : ${PIP_EXTRAS}"
-    "${PYTHON_BIN}" -m pip install -e "${ROOT_DIR}[${PIP_EXTRAS}]"
-else
-    "${PYTHON_BIN}" -m pip install -e "${ROOT_DIR}"
-fi
+echo "Extras          : ${INSTALL_EXTRAS}"
+"${PYTHON_BIN}" -m pip install -e "${ROOT_DIR}[${INSTALL_EXTRAS}]"
 
 if [[ "${INSTALL_TEST_DEPS}" == "1" ]]; then
     echo
